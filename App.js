20581a9ae084eccc21ce32a4b098f69b5f6d1f61
erikalad <desarrollo@qsocialnow.com>
2023-10-01 19:33:25 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Icon, WhiteSpace, WingBlank } from '@ant-design/react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WingBlank>
+    <WhiteSpace />
+    <Button>default</Button>
+    <WhiteSpace />
+    <Button disabled>default disabled</Button>
+    <WhiteSpace />
+
+    <Button type="primary">primary</Button>
+    <WhiteSpace />
+    <Button type="primary" disabled>
+      primary disabled
+    </Button>
+    <WhiteSpace />
+
+    <Button type="warning">warning</Button>
+    <WhiteSpace />
+    <Button type="warning" disabled>
+      warning disabled
+    </Button>
+    <WhiteSpace />
+
+    <Button loading>loading button</Button>
+
+    <Button activeStyle={false}>无点击反馈</Button>
+    <WhiteSpace />
+    <Button activeStyle={{ backgroundColor: 'red' }}>
+      custom feedback style
+    </Button>
+    <WhiteSpace />
+
+    <WingBlank
+      style={{
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+      <Button type="ghost">ghost</Button>
+      <Button type="ghost" disabled>
+        ghost disabled
+      </Button>
+      <Button type="ghost" size="small">
+        ghost
+      </Button>
+    </WingBlank>
+    <WhiteSpace />
+
+    <Button type="primary">
+      <Icon name="login" />
+    </Button>
+  </WingBlank>
   );
 }
 
@@ -18,3 +66,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
